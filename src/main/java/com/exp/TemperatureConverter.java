dc77c6249f643932c400300245dffd05d8d973b8
sri <sri.narayanan.gce@gmail.com>
2011-04-07 01:38:59 +0530

    Arquillian simple test against jboss 6

---
+++ src/main/java/com/exp/TemperatureConverter.java
@@ -0,0 +1,19 @@
+package com.exp;
+public class TemperatureConverter {
+
+
+   public double convertToCelsius(double f) {
+
+      return ((f - 32) * 5 / 9);
+
+   }
+
+
+   public double convertToFarenheit(double c) {
+
+      return ((c * 9 / 5) + 32);
+
+   }
+
+
+}
