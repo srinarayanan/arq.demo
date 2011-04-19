91e466d624309abb1ddd2ecd018c6f850184e220
srinarayanan <sri.narayanan.gce@gmail.com>
2011-04-19 23:45:58 +0530

    sip servlet - error

---
+++ src/main/java/com/exp/sip/TestSipServlet.java
@@ -0,0 +1,20 @@
+package com.exp.sip;
+import javax.servlet.ServletException;
+import javax.servlet.sip.SipServlet;
+
+/**
+ * TestSipServlet
+ *
+ * @author Jean Deruelle
+ * @version $Revision: $
+ */
+public class TestSipServlet extends SipServlet
+{
+   private static final long serialVersionUID = 1L;
+
+
+   @Override
+	public void init() throws ServletException {
+		super.init();
+	}
+}
---
+++ src/test/java/demo/sip/SipServletTest.java
@@ -0,0 +1,30 @@
+package demo.sip;
+
+import junit.framework.Assert;
+
+import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.sip.api.spec.ConvergedSipWebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.exp.sip.TestServlet;
+import com.exp.sip.TestSipServlet;
+
+@RunWith(Arquillian.class)
+public class SipServletTest {
+
+	@Deployment
+	public static ConvergedSipWebArchive createDeployment() {
+		return ShrinkWrap.create(ConvergedSipWebArchive.class, "test.war")
+				.addClass(TestServlet.class).addClass(TestSipServlet.class)
+				.addAsWebResource("client-web.xml", "web.xml")
+				.addAsWebResource("client-sip.xml", "sip.xml");
+	}
+
+	@Test
+	public void testConvertToFarenheit() {
+		Assert.assertEquals(1, 1);
+	}
+}
